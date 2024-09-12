7e189d2991e6222be9ca64717a7ce0a987879aa5
Senay Mustafa <senaim98@gmail.com>
2024-09-12 14:41:03 +0200

    index.js done

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-
-/*
-import fetch from 'node-fetch';
-
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-    .then((response) => response.text())
-    .then((div) => {
-      const htmlString = div
-      console.log(htmlString)
-    });
-*/
-
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { DownloaderHelper } from 'node-downloader-helper';
+
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+const folder ='./memes/';
 
 let emptyArray = [];
-function slice() {
-  emptyArray = emptyArray.slice(0, 11);
-}
+
+
 
 //init the axios with the url
 axios.get(url)
@@ -28,14 +18,33 @@ axios.get(url)
         // getting all the img with id #images in the html
         const images = '#images img';
         $(images).each((_, img) => {
-            // console.log({
-            // src: $(img).attr('src'),
-            //});
-            //pushing all the img in the empty array and slicing with the predefined function and logging
+          // pushing all the images into the array
             emptyArray.push($(img).attr('src'))
-            slice(emptyArray)
-            console.log(emptyArray)
         });
-
+        //returning the array to proceed into the next .then promise
+        return emptyArray
+     })
+     //slicing the array to include only first 10 and returning it
+   .then(emptyArray =>{
+    const tenArray = emptyArray.slice(0,10)
+    return tenArray
     })
-//console.log(emptyArray)
+    //using forEach to iterate over each URL and initiate the download
+    .then(tenArray => {
+    tenArray.forEach(downloadImage)
+    });
+
+const downloadImage = (imageURL, index) => {
+  //file naming based on index
+  const fileName = (index + 1).toString().padStart(2, 0) + '.jpg';
+  const options = { override: true, fileName: fileName };
+
+  const dl = new DownloaderHelper(imageURL, './memes', options);
+  //event lister to catch errors
+  dl.on('end', () =>
+    console.log(`Image ${fileName} has been successfully saved`),
+  );
+  dl.on('error', (err) => console.log('Download Failed', err));
+
+  dl.start().catch((err) => console.error(err));
+};
